e5a0e0b936ac2403c3df13997e63b97208bcde83
Duru-young Raymond <duruyoungcr@gmail.com>
2021-11-07 17:04:56 +0100

    New: Created Blog component

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { BlogItemsStyles } from "./BlogStyles"
+
+const Blog = () => {
+    return (
+        <BlogItemsStyles>
+            <h1>
+                Coming Soon!
+            </h1>
+        </BlogItemsStyles>
+    )
+}
+
+export default Blog
