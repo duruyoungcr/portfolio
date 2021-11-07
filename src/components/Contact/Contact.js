0d377073cf121ad45d96665e228dd9705bc98fe0
Duru-young Raymond <duruyoungcr@gmail.com>
2021-11-07 17:45:42 +0100

    Update: Added formspree for form submission

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,9 @@ import { ContactStyles } from "./ContactStyles"
 const Contact = () => {
   return (
     <ContactStyles>
-      <form name="contact">
+      <form name="contact" action="https://formspree.io/f/xwkrqjlk"
+        method="POST">
+        <input type="text" name="_gotcha" style={{ display: "none" }} />
         <input placeholder="Your name" type="text" name="name" minLength="3" required />
         <input placeholder="Your email" type="email" name="email" required />
         <textarea style={{ resize: 'none' }} placeholder="Your message" name="message" rows="6" required></textarea>
